545b62d0fa1ea8c092b410a3c2f1f07ada60ffc5
Scott Wadden <scott.wadden@gmail.com>
2020-09-10 18:59:03 -0300

    os.paramStr doesn't work when building as a lib

--- compiler/scriptconfig.nim
+++ compiler/scriptconfig.nim
@@ -141,10 +141,11 @@ proc setupVM*(module: PSym; cache: IdentCache; scriptName: string;
     setResult(a, options.existsConfigVar(conf, a.getString 0))
   cbconf nimcacheDir:
     setResult(a, options.getNimcacheDir(conf).string)
-  cbconf paramStr:
-    setResult(a, os.paramStr(int a.getInt 0))
-  cbconf paramCount:
-    setResult(a, os.paramCount())
+  when compiles(os.paramCount()):
+    cbconf paramStr:
+      setResult(a, os.paramStr(int a.getInt 0))
+    cbconf paramCount:
+      setResult(a, os.paramCount())
   cbconf cmpIgnoreStyle:
     setResult(a, strutils.cmpIgnoreStyle(a.getString 0, a.getString 1))
   cbconf cmpIgnoreCase:
